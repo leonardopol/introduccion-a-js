6875e933079d002c2a259fc8f6c75ce23daa86f1
leonardopol <leonardo_pol@hotmail.com>
2023-05-24 17:14:47 -0300

    refactorizacion de codigo

--- tareas/clase-5/tarea-clase-5.js
+++ tareas/clase-5/tarea-clase-5.js
@@ -22,6 +22,17 @@ $botonCalcular.onclick = function()
 //cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
 // en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
 
+document.querySelector("#calcular").onclick = function(){
+
+  const $salarioAnual = document.querySelector("#entrada").value;
+  const mesesDelAnio = 12;
+  const salarioMensual = $salarioAnual / mesesDelAnio;
+
+  document.querySelector("#salarioMensual").value = salarioMensual;
+
+  return false;
+}
+
 //TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
 // creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
 // también vamos a crear un <h1> que diga Bienvenido!
